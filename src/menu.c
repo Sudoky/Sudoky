65027d301ffa6b0e0829a1604790d7bb30178561
Minddarkness <miss.levinaa@yandex.ru>
2017-05-13 13:30:33 +0600

    All menu

--- src/menu.c
+++ src/menu.c
@@ -11,12 +11,13 @@ int main()
 	cout<<n;
 	system("PAUSE");
     return 0;
-}int Menu()
+}
+int Menu()
 {
 	int n=1, i;
     char ch;
     initwindow(1000,1000);
-    while (1)	
+    while (1)
 	{ 
     	do 
 		{ 
@@ -48,7 +49,7 @@ int main()
         } 
 		while (ch!=13);
         cleardevice();
-                switch(n)
+        switch(n)
         {
 			case 1: do 
 			{ 
@@ -79,7 +80,7 @@ int main()
           	}
 			while (ch!=13);
           	cleardevice();
-          				switch(n)
+			switch(n)
 		   	{
 				case 1: do 
 				{ 
@@ -150,7 +151,67 @@ int main()
           		} 
 				while (ch!=13);
           		cleardevice();
-    }   
-	system("PAUSE");
+       			switch(n) 
+          		{
+		  			case 1: return 121;
+          			case 2: return 122;
+				} 
+	  			break;
+               	
+				while (ch!=13);
+          		cleardevice();
+               	default: break;
+			}
+   			break;
+        	case 2:
+			{
+				do 
+				{ 
+   					setcolor(2);
+          			for (i=1; i<=3; i++)
+					{ 
+              			switch(i)
+              			{
+							case 1: outtextxy (300,100+i*100," Check Simple"); break;
+           					case 2: outtextxy (300,100+i*100," Check Diag"); break;
+                			case 3: outtextxy (300,100+i*100," Back"); break;
+                			default: break;
+               			}
+       				}
+              		setcolor(4);
+       				switch(n)
+       				{
+						case 1: outtextxy (300,100+n*100," Check Simple"); break;
+                		case 2: outtextxy (300,100+n*100," Check Diag"); break;
+           				case 3: outtextxy (300,100+n*100," Back"); break;
+           				default: break;
+					}
+          			ch=getch();
+       				if (ch==72) n--;
+          			if (n<=0) n=3;
+          			if (ch==80) n++;
+       				if (n>3) ;
+       		 	}
+				while (ch!=13);
+          		cleardevice();
+          		switch(n) 
+       			{
+	  				case 1: return 21;
+	       			case 2: return 22;
+	 				break;
+		  		}
+				n=1;
+		  		break;
+			}
+			break;
+           	case 3:
+			{ 
+			return 3;
+			}
+			break;
+            default: break;  
+		}
+	}
+    system("PAUSE");
     return 0;
 }
