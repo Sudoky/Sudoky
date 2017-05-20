e4a4c81b70171b6d338645c7ea945f5c8212217d
Minddarkness <miss.levinaa@yandex.ru>
2017-05-20 18:18:08 +0600

    +help

--- src/menu.cpp
+++ src/menu.cpp
@@ -9,7 +9,7 @@ int Menu()
     	do 
 		{ 
         	setcolor(2);
-        	for (i=1; i<=3; i++)
+        	for (i=1; i<=4; i++)
 		  	{
             	settextstyle(GOTHIC_FONT, HORIZ_DIR, 6);
             	switch(i)
@@ -17,6 +17,7 @@ int Menu()
 					case 1: outtextxy (100,50+i*100," Play"); break;
                 	case 2: outtextxy (100,50+i*100," Check"); break;
                 	case 3: outtextxy (100,50+i*100," Exit"); break;
+                	case 4: outtextxy (100,50+i*100," Help"); break;
                 	default: break;
                 }
         	}
@@ -26,13 +27,14 @@ int Menu()
 				case 1: outtextxy (100,50+n*100," Play"); break;
                	case 2: outtextxy (100,50+n*100," Check"); break;
                	case 3: outtextxy (100,50+n*100," Exit"); break;
+               	case 4: outtextxy (100,50+n*100," Help"); break;
               	default: break;
 			}
         	ch=getch();
         	if (ch==72) n--;
-        	if (n<=0) n=3;
+        	if (n<=0) n=4;
         	if (ch==80) n++;
-        	if (n>3) n=1 ;
+        	if (n>4) n=1 ;
         } 
 		while (ch!=13);
         cleardevice();
@@ -105,6 +107,7 @@ int Menu()
 	  				case 1: return 111;
 	       			case 2: return 112;
        				case 3: return 113;
+       				case 4: return 114;
 	 				break;
 		  		} 
 				n=1;
@@ -142,6 +145,7 @@ int Menu()
           		{
 		  			case 1: return 121;
           			case 2: return 122;
+          			case 3: return 123;
 				} 
 	  			break;
                	
@@ -185,6 +189,7 @@ int Menu()
        			{
 	  				case 1: return 21;
 	       			case 2: return 22;
+	       			case 3: return 23;
 	 				break;
 		  		}
 				n=1;
@@ -195,10 +200,60 @@ int Menu()
 			{ 
 			return 3;
 			}
-			break;
-            default: break;  
+			case 4:
+        	{	
+				setcolor(1);
+        		settextstyle(GOTHIC_FONT, HORIZ_DIR, 1);
+				outtextxy (10,10,"Rules of the game: ");
+				outtextxy (10,40,"to fill all empty cages so ");
+				outtextxy (10,70,"that figures didn't repeat ");
+				outtextxy (10,100,"in every line and a column,");
+				outtextxy (10,130,"and also squares 3 on 3.");
+				outtextxy (10,160,"If you play the diag mode,");
+				outtextxy (10,190,"then you should receive ");
+				outtextxy (10,220,"also not repeating on noted diagonals");
+				outtextxy (10,250,"(visible when input diag mode).");
+				outtextxy (10,280,"For check from file of your of a variant sudoky,");
+				outtextxy (10,310, "you enter in the 22.txt file");
+				outtextxy (10,340,"and you insert columns through gaps.");
+				outtextxy (10,370,"Example of filling of 3 columns");
+				outtextxy (10,400,"123456789 234567891 345678912.");
+       			setcolor(4);
+       			for (i=1; i<=1; i++)
+				{
+					settextstyle(GOTHIC_FONT, HORIZ_DIR, 6);
+           			switch(i)
+   					{
+						case 1: outtextxy (300,500," Back"); break;
+   						default: break;
+           			}
+            	}
+           		setcolor(4);
+       			switch(n)
+            	{
+					case 1: outtextxy (300,500," Back"); break;
+           			default: break;
+				}    
+          		ch=getch();
+   				if (ch==72) n--;
+          		if (n<=0) n=1;
+       			if (ch==80) n++;
+   				if (n>1) n=1;
+          	}
+			while (ch!=13);
+          	cleardevice();
+          	switch(n) 
+       		{
+	 			case 1: return 41;
+				break;
+	  		}
+				n=1;
+		  		break;
 		}
+		break;
 	}
+	
     system("PAUSE");
     return 0;
 }
+
